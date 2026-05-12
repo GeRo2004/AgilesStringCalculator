7a5dc13d3327bc894656c6080a4f37dc94d1e626
Facundo Munné <k50361@frro.utn.edu.ar>
2026-05-11 21:10:04 -0300

    numero devuelve numero

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ class calculator:
     def calculator(texto):
         if texto == "":
             return 0
-
+        return int(texto)
     #numeros = texto.split(",")
 
     #return sum(int(n) for n in numeros)
--- calculator_test.py
+++ calculator_test.py
@@ -2,4 +2,7 @@ import pytest as pytest
 from calculator import calculator
 
 def test_string_vacio_devuelve_0():
-    assert calculator.calculator("") == 0
+     assert calculator.calculator("") == 0
+
+def test_un_numero_devuelve_ese_numero():
+    assert calculator("1") == 1
