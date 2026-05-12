119d8ed311e13926326c3978c95dbc507ee9d61c
Geronimo Colman <gerocolman82@gmail.com>
2026-05-11 21:34:02 -0300

    Refactor, David

--- calculator.py
+++ calculator.py
@@ -1,14 +1,27 @@
 
 class calculator:
-    def calculator(texto):
-        if texto == "":
-            return 0
-        elif texto == "1":
-            return 1
-        elif texto == "1,2":
-            return 3
-        return texto
+    #def calculator(texto):
+    #    if texto == "":
+    #        return 0
+    #    elif texto == "1":
+    #        return 1
+    #    elif texto == "1,2":
+    #        return 3
+    #    elif texto == "3,5":
+    #        return 8
+    #    return texto
     
     #numeros = texto.split(",")
 
     #return sum(int(n) for n in numeros)
+
+    #Refactor luego de detectar el patron de sumas
+    def calculator(texto):
+        if texto == "":
+            return 0
+        numeros = texto.split(",")
+        if len(numeros) == 1:
+            return int(texto)
+        else:
+            num1, num2 = numeros
+            return int(num1) + int(num2)
--- calculator_test.py
+++ calculator_test.py
@@ -9,3 +9,6 @@ def test_un_numero_devuelve_ese_numero():
 
 def test_dos_numeros_devuelve_suma():
     assert calculator.calculator("1,2") == 3
+
+def test_tres_cinco_devuelve_ocho():
+    assert calculator.calculator("3,5") == 8
